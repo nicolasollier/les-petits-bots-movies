ed49b0d5e7160331a2359b11d11db6f456101dd7
Nicolas Ollier <nicolas.f.ollier@gmail.com>
2022-04-19 16:17:25 +0200

    fetch the last movie in list

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,24 +1,42 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Movie = () => {
-  const getMovies = async () => {
+  const [movies, setMovies] = useState([]);
+
+  const getLastMovies = async () => {
     try {
+      // API Call (proxy setup in package.json)
       const res = await axios.get("movies");
       const datas = res.data;
 
-      return datas;
-      
+      // Modify here to selected more items (now limited to last)
+      let movie = [datas[datas.length - 1]];
+
+      setMovies(movie);
+
     } catch (error) {
       throw new Error(error);
     }
   };
 
   useEffect(() => {
-    getMovies();
+    getLastMovies();
   }, []);
 
-  return <div>Movie</div>;
+  return (
+    <>
+      {movies.map((movie) => {
+        return (
+          <div key={movie.id}>
+            <p>{movie.id}</p>
+            <p>{movie.name}</p>
+            <p>{movie.description}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Movie;
