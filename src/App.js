3882617fdc2d528058facab127183433f9cd337a
Nicolas Ollier <nicolas.f.ollier@gmail.com>
2022-04-19 12:49:54 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import MoviePoster from './components/Movie/MoviePoster';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+    <div className="main-wrapper">
+      <MoviePoster />
     </div>
   );
 }
