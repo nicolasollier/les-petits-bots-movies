6c8a133bee65977a764ff29075ecd9c277ca0d16
Nicolas Ollier <nicolas.f.ollier@gmail.com>
2022-04-19 18:22:44 +0200

    add movieId

--- src/components/Movies/MovieId/MovieId.jsx
+++ src/components/Movies/MovieId/MovieId.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Card from "../../UI/Card/Card";
 
 //Custom classes
-import classes from "./MovieId.module.css"
+import classes from "./MovieId.module.css";
 
 const MovieId = ({ movieId }) => {
   return (
     <Card>
-      <span className={classes.id}>ID</span>
-      <p>#{movieId}</p>
+      <span className={classes.subheading}>ID</span>
+      <p className={classes.movieId}>#{movieId}</p>
     </Card>
   );
 };
